262f60ba3cd2d91851a6889868027e0dc6096816
fatimamjk <fatimam5868@gmail.com>
2025-07-16 20:20:14 +0500

    Updated App.tsx layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   const [refresh, setRefresh] = useState(false);
   return (
     <div className="max-w-2xl mx-auto p-6">
-      <h1 className="text-3xl font-bold mb-6 text-center text-blue-700">
+      <h1 className="text-3xl font-bold mb-6 text-center text-black">
         🔗 URL Shortener
       </h1>
       <UrlForm onUrlCreated={() => setRefresh(!refresh)} />
